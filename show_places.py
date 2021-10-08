79ad9d0f3ce1a88b6261b8f235a1456b877aac67
point48 <aeroflot.ua@gmail.com>
2021-10-08 14:08:14 +0300

    Initial for Telegram Bot

--- show_places.py
+++ show_places.py
@@ -1,9 +1,12 @@
 import csv
 import mpu
+import telebot
+
+bot = telebot.TeleBot("2042323681:AAGlGm683i4jAEEFoatxZl9tpUzfZCtlruo", parse_mode=None)
 
 lat1 = 50.44029
 lon1 = 30.55950
-min_geo_range = 20
+min_geo_range = 30
 max_geo_range = 50
 
 rows = {}
@@ -15,6 +18,19 @@ for elem in csv_reader:
     rows[elem[2]] = distance
 file.close()
 sorted_rows = dict(sorted(rows.items(), key=lambda x: x[1]))
+final_dict = {}
 for k, v in sorted_rows.items():
     if min_geo_range <= v <= max_geo_range:
-        print(k, round(v, 1))
+        # print(k, round(v, 1))
+        final_dict[k] = round(v, 1)
+
+final_string = ("\n".join("{!r} - {!r}".format(k, v)
+                          for k, v in final_dict.items())).replace("'", "").replace('"', '')
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, final_string)
+
+
+bot.infinity_polling()
